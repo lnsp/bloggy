908bff701cd5a17ece4a00d39c28f3a88037f7c9
Lennart Espe <lennart@mooxmirror.io>
2016-05-05 12:17:22 +0200

    Fix code

--- hsts.go
+++ hsts.go
@@ -3,9 +3,9 @@ package main
 import "net/http"
 
 // Middleware to add HSTS header
-func hstsHandler(fn http.Handler) http.Handler {
+func hstsHandler(h http.Handler) http.Handler {
     return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Strict-Transport-Security", "max-age=31536000; preload")
-        fn.ServeHTTP(w, r)
+        h.ServeHTTP(w, r)
     })
 }
