c5c0507ffa2894e4c56756ca0750cfe648b3a80c
Lennart Espe <lennart@mooxmirror.io>
2016-05-24 18:48:35 +0200

    Add sub domain HTTPS enhancement

--- hsts.go
+++ hsts.go
@@ -4,8 +4,8 @@ import "net/http"
 
 // Middleware to add HSTS header
 func hstsHandler(h http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Strict-Transport-Security", "max-age=31536000; preload")
-        h.ServeHTTP(w, r)
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
+		h.ServeHTTP(w, r)
+	})
 }
